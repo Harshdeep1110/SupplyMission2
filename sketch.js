5a8c7eba55617a007d6601b4290af4b1d786fc67
Harshdeep1110 <74968360+Harshdeep1110@users.noreply.github.com>
2020-12-18 11:08:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var container, containerBody, container2, container3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	container = createSprite(400,650,200,20);
+	container.shapeColor = "red";
+
+	container2 = createSprite(300,610,20,100);
+	container2.shapeColor = "red";
+
+	container3 = createSprite(500,610,20,100);
+	container3.shapeColor = "red";
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.0, isStatic:true});
+	World.add(world, packageBody);
+
+	containerBody = Bodies.rectangle(400,630,200,20 , {isStatic:true} );
+	World.add(world, containerBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(25);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  
+
+  
+  if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+	
+	Matter.Body.setStatic(packageBody, false);
+	
+	
+	
+  }
+
+ 
+  
+  drawSprites();
+ 
+}
+
+
+
+
+
